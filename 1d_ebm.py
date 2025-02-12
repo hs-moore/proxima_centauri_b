2ad14ca0ed1f014a838c776a3950be556af10b2b
hs-moore <heather.sm555@gmail.com>
2025-02-12 17:29:11 +0000

    just loads on atmospheric staiblity and varying the bloody diffusion constant

--- 1d_ebm.py
+++ 1d_ebm.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 
-def get_solar(z):
+def get_solar(z, q3):
     if z > np.pi/2:
         s = 0
     else:
         s = q2*np.cos(z)
+        #s = q3/(90/lon_step)
     return s
 
 def get_albedo(tij):
@@ -34,21 +35,34 @@ def get_cheat(tij, f_ocean):
     c = (1-f_ocean)*c_land + f_ocean*((1-f_ice)*c_ocean + f_ice*c_ice)
     return c
 
-#constants#
-c_land = 5.25e6
-c_ocean = 40*c_land
-g = 6.67e-11
-a = 0.04856*1.5e11
-q = (0.00155*382.8e24)/(4*np.pi*a**2)
-q2 = (0.00155*1360)/(0.04856**2)
-period = 11.1868*86400
-#diff = 5.394e-1 * 11.1868**(-2)
-diff = 0.02 * 11.1868**(-1)
-#diff = ((1/11.1868)*4.30130890052356*-1 + 1.78111335078534)#*(1/11.1868)
-diff = 30*diff
-print(diff)
+def get_toa(tij, lon):
 
-def run(lon_step, time_step, n_days, fo):
+    u = np.cos(lon)
+    p = pressure * pco2 * 10**(-6)
+    a = get_albedo(tij)
+
+    if tij < 190:
+        tij = 190
+    if tij > 370:
+        tij = 370
+
+    if tij < 280:
+        toa = -0.6891 + 1.0468*a + 0.007845*tij -0.0028373*p -0.28899*u
+        toa += -0.037412*a*p - 0.0063499*u*p + 0.20122*a*u
+        toa += -0.0018508*a*tij + 0.00013649*u*tij + 9.8581e-5*p*tij
+        toa += 0.073239*a**2 - 1.6555e-5*tij**2 + 6.5817e-4*p**2 + 0.081218*u**2
+
+    else:
+        toa = 1.1082 + 1.5172*a - 5.7993e-3*tij +0.019705*p - 0.1867*u
+        toa += -0.031355*a*u - 0.010214*u*p + 0.20986*a*u
+        toa += -3.7098e-3*a*tij -1.1335e-4*u*tij +5.3714e-5*p*tij 
+        toa += 0.075887*a**2 +9.269e-6*tij**2 - 4.1327e-4*p**2 + 0.063298*u**2
+
+    return toa
+
+#########################################
+
+def run(lon_step, time_step, n_days, fo, m0):
 
     #initialise
     times = np.arange(start=0, stop=n_days, step=time_step)
@@ -59,16 +73,25 @@ def run(lon_step, time_step, n_days, fo):
     temperature = np.zeros((len(lon_rad), len(times)))
     temperature[:, 0] = 350
 
+    q3 = 0
+    for i in range(0, 18):
+        q3 += q2 * np.cos(lon_rad[i])
+    print(q2, q3)
     #run through time
     for i in range(0, len(times)-1):
         for j in range(0, len(lon_rad)):
             tij = temperature[j, i]
             lon = lon_rad[j]
 
-            solar = get_solar(lon)
-            albedo = get_albedo(tij)
+            solar = get_solar(lon, q3)
+            #albedo = get_albedo(tij)
+            albedo = get_toa(tij, lon)
             infra = get_infra(tij)
             c_heat = get_cheat(tij, fo)
+
+            t_avg = np.average(temperature[:, i])
+            diff = 0.02 * pressure * 11.1868**(-1) * (m0/29)**2 * (t_avg/288)**12
+            diff = d_mult*diff/t_adv
 
             if j==len(lon_rad)-1: #longitude +90
                 first_deriv = 0
@@ -89,13 +112,35 @@ def run(lon_step, time_step, n_days, fo):
 
     return temperature
 
-time_step = 0.5
-n_days = 1000
+n2_cp = {100:[64.4, 56.3], 200:[30.4, 45.5], 300:[29.6, 33.4]}
+co2_cp = {100:[37.1], 200:[37.1], 300:[37.2]}
+
+time_step = 0.05
+n_days = 250
 lon_step = 5
 fo = 0.7
 
-r = run(lon_step, time_step, n_days, fo)
+#constants#
+c_land = 5.25e6
+c_ocean = 40*c_land
+g = 6.67e-11
+a = 0.04856*1.5e11
+q = (0.00155*382.8e24)/(4*np.pi*a**2)
+q2 = (0.00155*1360)/(0.04856**2)
+period = 11.1868*86400
+pressure = 10 #in bar
+pco2 = 300 # in ppm
+m0 = 44
+d_mult = 1
+#diff = ((1/11.1868)*4.30130890052356*-1 + 1.78111335078534)#*(1/11.1868)
+rp = 0.5*6371e3
+t_adv = (rp/1e3) / (time_step * period)
+print(t_adv)
+#
+#print(diff)
+
+r = run(lon_step, time_step, n_days, fo, m0)
 df = pd.DataFrame(r)
-filename = "diff_30.csv"
+filename = "tadv_0.5re_co2_p" + str(pressure) + '_d' + str(d_mult) + "_i.csv"
 df.to_csv(filename)
 
--- plot_templines.py
+++ plot_templines.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-nums = [1, 10, 20, 30]
+files = ['1re_co2_p10_d1', '2re_co2_p10_d1', '0.5re_co2_p10_d1', '1re_co2_p10_d2', '2re_co2_p10_d2', '0.5re_co2_p10_d2']
 
 diffs = []
-for i in nums:
-    df = pd.read_csv('diff_' + str(i) + '.csv')
+for i in files:
+    df = pd.read_csv('tadv_' + str(i) + '_ilons.csv')
     temperature = df.to_numpy()
     temperature = temperature[:, 1:]
     temp = temperature[:, -1]
@@ -18,14 +18,23 @@ for i in nums:
 lon_step = int(180/(lons-1))
 lons_rad = np.arange(start=-180+lon_step/2, stop=180+lon_step/2, step=lon_step)
 
-for i in range(0, len(diffs)):
-    plt.plot(lons_rad, diffs[i], label=nums[i])
+fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
 
-plt.plot([-180, 180], [273, 273], ls='dashed', color='black')
-plt.plot([-180, 180], [373, 373], ls='dashed', color='black')
-plt.xlabel('Longitude ($\circ$)')
-plt.ylabel('Temperature ($K$)')
-plt.legend()
+for i in range(0, len(diffs)):
+    if i < 3:
+        ax1.plot(lons_rad, diffs[i], label=files[i])
+    else:
+        ax2.plot(lons_rad, diffs[i], label=files[i])
+
+ax1.plot([-180, 180], [273, 273], ls='dashed', color='black')
+ax1.plot([-180, 180], [453, 453], ls='dashed', color='black')
+ax2.plot([-180, 180], [273, 273], ls='dashed', color='black')
+ax2.plot([-180, 180], [453, 453], ls='dashed', color='black')
+fig.supxlabel('Longitude ($\circ$)')
+fig.supylabel('Temperature ($K$)')
+ax1.legend()
+ax2.legend()
+fig.suptitle('I/lons')
 #plt.plot([90, lons/2], [min(temp), max(temp)], ls='dashed', color='black')
-plt.savefig('diffs_temps.png')
+plt.savefig('tadv_d_temps_ilons.png')
 plt.show()
